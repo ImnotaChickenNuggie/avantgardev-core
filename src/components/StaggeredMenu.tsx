3c07f3cc56cd5555c1aac4a1f8fcaf934d4763c9
Bruno <brunohdzv@gmail.com>
2026-04-24 16:37:42 -0600

    fix: correccion de bug en ventana mobile
    - Se delimita tamaño de menu que no permitia interactuar con el sitio

--- src/components/StaggeredMenu.tsx
+++ src/components/StaggeredMenu.tsx
@@ -409,7 +409,7 @@ export const StaggeredMenu: React.FC<StaggeredMenuProps> = ({
 
 	return (
 		<div
-			className={`sm-scope z-[60] ${isFixed ? "fixed top-0 left-0 w-screen h-screen overflow-hidden" : "w-full h-full"}`}
+			className={`sm-scope z-[60] ${isFixed ? `fixed top-0 left-0 w-screen h-screen overflow-hidden${!open ? " pointer-events-none" : ""}` : "w-full h-full"}`}
 		>
 			<div
 				className={`${className ? `${className} ` : ""}staggered-menu-wrapper pointer-events-none relative w-full h-full z-40`}
